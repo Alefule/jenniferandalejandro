f3d0a04fdd4203827e035149cf1e2e7a3dde951b
Alejandro Fuentes <alesetneuf@gmail.com>
2025-03-24 18:40:12 +0000

    First approach with invitations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/Alefule/jenniferandalejandro/',
-    base: '/jenniferandalejandro/', 
-    output: 'static' 
+  site: 'https://github.com/Alefule/jenniferandalejandro/',
+  base: '/',
+  output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
