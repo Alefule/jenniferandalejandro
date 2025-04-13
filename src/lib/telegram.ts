6d9c800e700b81db8f58ddaf6c86a893d8071b14
Alejandro Fuentes <alesetneuf@gmail.com>
2025-04-13 23:52:43 +0100

    delete console.log

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -3,8 +3,7 @@ export const sendTelegramMessage = async (message: string) => {
     const chatId = import.meta.env.TELEGRAM_CHAT_ID;
   
     const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
-    console.log(url)
-  
+
     await fetch(url, {
       method: 'POST',
       headers: {
